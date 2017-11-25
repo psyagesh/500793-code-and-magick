1c154732058b7ab1e0ff4a18e3c3eb429da4e950
Andrey Prokhorov <30868526+psyagesh@users.noreply.github.com>
2017-11-25 05:25:27 +0300

    Выполнил третье задание

---
+++ js/setup.js
@@ -0,0 +1,89 @@
+'use strict';
+var WIZARD_SIMILAR = 4;
+var WIZARD_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+var WIZARD_SURNAMES = [
+  ' да Марья',
+  ' Верон',
+  ' Мирабелла',
+  ' Вальц',
+  ' Онопко',
+  ' Топольницкая',
+  ' Нионго',
+  ' Ирвинг'
+];
+var COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+var wizards = [
+  {
+    name: '',
+    coatColor: '',
+    eyesColor: ''
+  },
+  {
+    name: '',
+    coatColor: '',
+    eyesColor: ''
+  },
+  {
+    name: '',
+    coatColor: '',
+    eyesColor: ''
+  },
+  {
+    name: '',
+    coatColor: '',
+    eyesColor: ''
+  }
+];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+for (var i = 0; i < WIZARD_SIMILAR; i++) {
+  wizards[i].name = WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] + WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_SURNAMES.length)];
+  wizards[i].coatColor = COAT_COLORS[Math.floor(Math.random() * COAT_COLORS.length)];
+  wizards[i].eyesColor = EYES_COLORS[Math.floor(Math.random() * EYES_COLORS.length)];
+}
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var j = 0; j < wizards.length; j++) {
+  fragment.appendChild(renderWizard(wizards[j]));
+}
+similarListElement.appendChild(fragment);
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
